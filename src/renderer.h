0337e41593a4b1f7a78e61fb15bffc02d9e7a8e8
Arya Prasetya <arya.prasetya@tu-berlin.de>
2024-06-14 12:46:52 +0200

    renderer class

--- src/renderer.h
+++ src/renderer.h
@@ -3,22 +3,29 @@
 
 #include "solver.h"
 #include <queue>
+#include <thread>             // For std::thread
+#include <mutex>              // For std::mutex, std::lock_guard, std::unique_lock
+#include <condition_variable> // For std::condition_variable
+#include <atomic>             // For std::atomic
 
 template <typename T>
 class Renderer
 {
 protected:
     Solver<T> &solver_;
-    std::queue<T *> input_buff;
-    T *output;
+    std::queue<T *> buffer_;
+    std::mutex buffer_mutex_;
+    std::condition_variable buffer_cv_;
+    std::atomic<bool> stop_flag_{false};
+
+    virtual void load_data() = 0;
+    void process_data(T *output);
+    void add_data(const T *input);
 
 public:
-    Renderer(Solver<T> &solver, int &dataframes) : solver_(solver){};
+    Renderer(Solver<T> &solver) : solver_(solver){};
     ~Renderer();
-    T *run()
-    {
-        output = (T *)calloc(p * dataframes, sizeof(double));
-    }
+    void render(T *output);
 };
 
 /// @brief Random Renderer.
@@ -26,7 +33,7 @@ template <typename T>
 class RandomRenderer : public Renderer<T>
 {
 public:
-    RandomRenderer(Solver<T> &solver, int &dataframes);
+    RandomRenderer(Solver<T> &solver);
     ~RandomRenderer();
     void feed_data();
 };
