8fa5fbf80ba4088601623b660f733458f41cdcf2
Arya Prasetya <arya.prasetya@tu-berlin.de>
2024-06-19 00:33:04 +0200

    add random input renderer

---
+++ tests/test_renderer.cpp
@@ -0,0 +1,49 @@
+#include "../cnpy/cnpy.h"
+#include "../src/state_space_system.h"
+#include "../src/solver.h"
+#include "../src/utils.h"
+#include "../src/renderer.h"
+
+int main(int argc, char const *argv[])
+{
+    std::string filename = argv[1];
+    cnpy::NpyArray A_npy = cnpy::npz_load(filename, "A");
+    cnpy::NpyArray B_npy = cnpy::npz_load(filename, "B");
+    cnpy::NpyArray C_npy = cnpy::npz_load(filename, "C");
+    cnpy::NpyArray D_npy = cnpy::npz_load(filename, "D");
+    cnpy::NpyArray input = cnpy::npz_load(filename, "input");
+
+    auto dataframes = input.shape[1];
+    auto n = A_npy.shape[0], m = B_npy.shape[1], p = C_npy.shape[0];
+    using T = double;
+    using sol = XGEMVSolver<T>;
+
+    std::cout << "dataframes: " << dataframes << std::endl;
+    std::cout << "n: " << n << std::endl;
+    std::cout << "m: " << m << std::endl;
+    std::cout << "p: " << p << std::endl;
+
+    StateSpaceSystem<T> system(A_npy.data<T>(), B_npy.data<T>(), C_npy.data<T>(), D_npy.data<T>(), A_npy.shape[0], B_npy.shape[1], C_npy.shape[0]);
+    sol solver(system, dataframes);
+    RandomRenderer<T> renderer(solver);
+
+    // Atomic flag to indicate when rendering is done
+    std::atomic<bool> rendering_done{false};
+
+    // Register the callback function
+    renderer.register_output_callback([&](T *output)
+                                      {
+        print_data(output, p, dataframes);
+        free(output); });
+
+    // Start the rendering process in a separate thread
+    std::thread render_thread([&]()
+                              {
+        renderer.render();
+        rendering_done.store(true); });
+
+    // Ensure the rendering thread has finished
+    render_thread.join();
+
+    return 0;
+}
