61dcfb4cae742f06d7c3dd0542d9ee1a875fb129
Arya Prasetya <arya.prasetya@tu-berlin.de>
2024-06-25 14:47:49 +0200

    add matrix structure enums

--- tests/test_system.cpp
+++ tests/test_system.cpp
@@ -13,7 +13,7 @@ public:
     {
     }
 
-    NoProcess(T *A, T *B, T *C, T *D, int n, int m, int p) : StateSpaceSystem<T>(A, B, C, D, n, m, p)
+    NoProcess(T *A, T *B, T *C, T *D, int n, int m, int p, MatrixStructure General) : StateSpaceSystem<T>(A, B, C, D, n, m, p, General)
     {
     }
 };
@@ -37,11 +37,11 @@ int main(int argc, char const *argv[])
     zero_state_float.info();
 
     std::cout << "- STL double Cnpy import - " << std::endl;
-    NoProcess<double> nonempty_state_double(A_npy.data<double>(), B_npy.data<double>(), C_npy.data<double>(), D_npy.data<double>(), A_npy.shape[0], B_npy.shape[1], C_npy.shape[0]);
+    NoProcess<double> nonempty_state_double(A_npy.data<double>(), B_npy.data<double>(), C_npy.data<double>(), D_npy.data<double>(), A_npy.shape[0], B_npy.shape[1], C_npy.shape[0], General);
     nonempty_state_double.info();
 
     std::cout << "- STL float Cnpy import - *Bug in cnpy* " << std::endl;
     // Note that you have to be careful when casting datatypes using cnpy, since float in python is internally casted as double. As you can see here, the value does not coincide correctly.
-    NoProcess<float> nonempty_state_float(A_npy.data<float>(), B_npy.data<float>(), C_npy.data<float>(), D_npy.data<float>(), A_npy.shape[0], B_npy.shape[1], C_npy.shape[0]);
+    NoProcess<float> nonempty_state_float(A_npy.data<float>(), B_npy.data<float>(), C_npy.data<float>(), D_npy.data<float>(), A_npy.shape[0], B_npy.shape[1], C_npy.shape[0], General);
     nonempty_state_float.info();
 }
