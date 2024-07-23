fc49d97e339a62580f42725bb81121d313824a73
Arya Prasetya <arya.prasetya1997@gmail.com>
2024-07-23 10:36:22 +0200

    merge fix

--- src/solver.cpp
+++ src/solver.cpp
@@ -5,11 +5,9 @@
 template <typename T>
 int Solver<T>::set_buffer_size(const int &buffer_size)
 {
-    std::cout << buffer_size << " solver "  << std::endl;
     this->buffer_size_ = buffer_size;
     return 0;
 }
-
 
 /* CPP-based solver */
 template <typename T>
